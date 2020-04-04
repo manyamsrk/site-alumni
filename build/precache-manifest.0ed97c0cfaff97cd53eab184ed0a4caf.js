6d7de8397f69559833c0a9e36792329d73396b9d
manyamsrk <45237487+manyamsrk@users.noreply.github.com>
2020-04-04 20:01:41 +0530

    Add files via upload

---
+++ build/precache-manifest.0ed97c0cfaff97cd53eab184ed0a4caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd794960d33276ee5e2",
+    "url": "/static/css/main.25083d64.chunk.css"
+  },
+  {
+    "revision": "1dd794960d33276ee5e2",
+    "url": "/static/js/main.1bd065e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66e818484007bd548cda",
+    "url": "/static/js/2.836069dc.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "4e7362b3b9a27fde6fb3c2a821489841",
+    "url": "/static/media/home.4e7362b3.png"
+  },
+  {
+    "revision": "3c9d65b048fe857fadb9f9c847c09ff6",
+    "url": "/index.html"
+  }
+];
