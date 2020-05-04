2ec7608eec49b74a13a9a535400bf542f39e4f2c
manyamsrk <45237487+manyamsrk@users.noreply.github.com>
2020-05-04 20:26:52 +0530

    Add files via upload

---
+++ site-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCKavLA0zQcCMw-oD_92B2j2c1NT5agF5Q",
+  authDomain: "social-7b361.firebaseapp.com",
+  databaseURL: "https://social-7b361.firebaseio.com",
+  projectId: "social-7b361",
+  storageBucket: "social-7b361.appspot.com",
+  messagingSenderId: "93374967710",
+  appId: "1:93374967710:web:c7817939ff8bba33f0b08f",
+  measurementId: "G-RDEB1XJPHK"
+};
